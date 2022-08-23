90d64a3897f04c976584d446b8a201b7fd7a6f88
SSGR18483 <gal18483@uvg.edu.gt>
2022-08-23 10:45:57 -0600

    improvement proto02v1
    se agrego programacion para poder trabajar con video en vivo por webcam

--- Prototipos/Proto_02V01.py
+++ Prototipos/Proto_02V01.py
@@ -2,4 +2,31 @@
 #Facultad de Ingenieria
 #Diseño e innovación
 #Santiago Galicia - UVG18483
-#Programa de prueba para determinar funcionalidad de procesamiento en vivo de video
+##Programa de prueba para determinar funcionalidad de procesamiento en vivo de video
+
+#librerias
+import numpy as np
+import cv2 as cv
+
+
+#programa
+cap= cv.VideoCapture(0)
+
+#proteccion
+if not cap.isOpened():
+    print('no se pudo abrir la camara')
+    exit()
+while True:
+    #Captura frame por frame
+    ret, frame = cap.read()
+
+    #proteccion de lectura de frame
+    if not ret:
+        print('no se pudo recivir frame. ')
+        break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('frame',gray)
+    if cv.waitKey(1)==ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
