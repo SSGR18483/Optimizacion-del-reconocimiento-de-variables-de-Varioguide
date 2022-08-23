d2ca463cd8b3f16d66617b2a9f2c3630775f012f
SSGR18483 <gal18483@uvg.edu.gt>
2022-08-23 09:49:32 -0600

    prototipo v2
    creacion archivo de prueba para procesamiento de video

---
+++ Prototipos/Proto_02V01.py
@@ -0,0 +1,5 @@
+#Universidad del Valle de Guatemala
+#Facultad de Ingenieria
+#Diseño e innovación
+#Santiago Galicia - UVG18483
+#Programa de prueba para determinar funcionalidad de procesamiento en vivo de video
