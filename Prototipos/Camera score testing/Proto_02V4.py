05aa17eaf9d2b21625177a8d80f4492629464c5e
SSGR18483 <gal18483@uvg.edu.gt>
2022-09-07 12:27:31 -0600

    se hicieron cambios del prototipo

--- Prototipos/Camera score testing/Proto_02V4.py
+++ Prototipos/Camera score testing/Proto_02V4.py
@@ -2,16 +2,18 @@
 import keras
 import numpy as np
 import cv2 as cv
+import glob
 import tensorflow as tf
 from matplotlib import pyplot
 from matplotlib import rcParams
 from scipy import io
 
 caso = 1
-Entrenamiento = 0
-LabelsEntrenamiento = 0
-Prueba = 0
-LabelsPrueba = 0
+Entrenamiento = []
+LabelsEntrenamiento = []
+Prueba = []
+LabelsPrueba = []
+for i in
 def model1():
     layer1=tf.keras.layers.Dense(5,activation='sigmoid',kernel_inizializer='he_uniform')
     model=tf.keras.models.Sequential()
@@ -23,67 +25,22 @@ def model1():
     history = model.fit(Entrenamiento, LabelsEntrenamiento, validation_data=(Prueba, LabelsPrueba), epochs=30)
 
     return history
-model1()
-if caso==0:
-    exit()
-elif caso ==1:
-    _, train_acc = model.evaluate(EdgePTrain, EdgeLTrain, verbose=0)
-    _, val_acc = model.evaluate(EdgePTrain, EdgeLTest, verbose=0)
-    print('Train: %.3f, Test: %.3f' % (train_acc, val_acc))
-    # Se grafica la evolución de la pérdida durante el entrenamiento y la
-    # validación
-    pyplot.subplot(211)
-    pyplot.title('Loss')
-    pyplot.plot(history.history['loss'], label='train')
-    pyplot.plot(history.history['val_loss'], label='test')
-    pyplot.legend()
-    # Se grafica la evolución de la exactitud durante el entrenamiento y la
-    # validación
-    pyplot.subplot(212)
-    pyplot.title('Accuracy')
-    pyplot.plot(history.history['accuracy'], label='train')
-    pyplot.plot(history.history['val_accuracy'], label='test')
-    pyplot.legend()
-    pyplot.show()
 
-elif caso ==2:
-    _, train_acc = model.evaluate(ThPTrain, ThLTrain, verbose=0)
-    _, val_acc = model.evaluate(ThPTest, ThLTest, verbose=0)
-    print('Train: %.3f, Test: %.3f' % (train_acc, val_acc))
-    # Se grafica la evolución de la pérdida durante el entrenamiento y la
-    # validación
-    pyplot.subplot(211)
-    pyplot.title('Loss')
-    pyplot.plot(history.history['loss'], label='train')
-    pyplot.plot(history.history['val_loss'], label='test')
-    pyplot.legend()
-    # Se grafica la evolución de la exactitud durante el entrenamiento y la
-    # validación
-    pyplot.subplot(212)
-    pyplot.title('Accuracy')
-    pyplot.plot(history.history['accuracy'], label='train')
-    pyplot.plot(history.history['val_accuracy'], label='test')
-    pyplot.legend()
-    pyplot.show()
-elif caso ==3:
-    _, train_acc = model.evaluate(LinesPTrain, LinesLTrain, verbose=0)
-    _, val_acc = model.evaluate(LinesPTest, LinesLTest, verbose=0)
-    print('Train: %.3f, Test: %.3f' % (train_acc, val_acc))
-    # Se grafica la evolución de la pérdida durante el entrenamiento y la
-    # validación
-    pyplot.subplot(211)
-    pyplot.title('Loss')
-    pyplot.plot(history.history['loss'], label='train')
-    pyplot.plot(history.history['val_loss'], label='test')
-    pyplot.legend()
-    # Se grafica la evolución de la exactitud durante el entrenamiento y la
-    # validación
-    pyplot.subplot(212)
-    pyplot.title('Accuracy')
-    pyplot.plot(history.history['accuracy'], label='train')
-    pyplot.plot(history.history['val_accuracy'], label='test')
-    pyplot.legend()
-    pyplot.show()
-else:
-    print('Ingrese un caso valido para calificar cámara(del 1 al 3)')
-
+_, train_acc = model.evaluate(LinesPTrain, LinesLTrain, verbose=0)
+_, val_acc = model.evaluate(LinesPTest, LinesLTest, verbose=0)
+print('Train: %.3f, Test: %.3f' % (train_acc, val_acc))
+# Se grafica la evolución de la pérdida durante el entrenamiento y la
+# validación
+pyplot.subplot(211)
+pyplot.title('Loss')
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+# Se grafica la evolución de la exactitud durante el entrenamiento y la
+# validación
+pyplot.subplot(212)
+pyplot.title('Accuracy')
+pyplot.plot(history.history['accuracy'], label='train')
+pyplot.plot(history.history['val_accuracy'], label='test')
+pyplot.legend()
+pyplot.show()
