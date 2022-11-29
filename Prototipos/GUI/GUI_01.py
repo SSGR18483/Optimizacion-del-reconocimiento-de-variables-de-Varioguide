55b91efdb10bcc18f69a3ab5d81587a37a0c09d1
SSGR18483 <gal18483@uvg.edu.gt>
2022-11-29 15:53:08 -0600

    Update imagenes editadas
    se generaron las imagenes editadas para hacer pruebas con distintos numeros

--- Prototipos/GUI/GUI_01.py
+++ Prototipos/GUI/GUI_01.py
@@ -15,8 +15,8 @@ import os
 root = Tk()
 root.geometry("1000x750")
 
-#img=PIL.Image.open("D:/Documentos/UVG/QUINTO AÑO/Segundo Semestre/Diseño e innovación/GIT/Optimizacion-del-reconocimiento-de-variables-de-Varioguide/Prototipos/Imagenes/Imagenes_gui/brain.jpg")
-img=PIL.Image.open("C:/Users/galic/Documents/Diseño/GIT/Trabajo-de-Graduaci-n-SG18483/Prototipos/Imagenes/Imagenes_gui/brain.jpg")
+img=PIL.Image.open("D:/Documentos/UVG/QUINTO AÑO/Segundo Semestre/Diseño e innovación/GIT/Optimizacion-del-reconocimiento-de-variables-de-Varioguide/Prototipos/Imagenes/Imagenes_gui/brain.jpg")
+# img=PIL.Image.open("C:/Users/galic/Documents/Diseño/GIT/Trabajo-de-Graduaci-n-SG18483/Prototipos/Imagenes/Imagenes_gui/brain.jpg")
 bg = ImageTk.PhotoImage(img)
 
 #funciones de los clicks
@@ -34,12 +34,15 @@ canvas1.create_text(500, 25, text="Sistema OCR HUMANA ",font =("Courier", 30,'bo
 
 # Create Buttons
 
-button1 = Button(root, text="Capturar Valor")
+button1 = Button(root, text="Determinar tipo de Junta")
 button1.place(x=50,y=50,width=100,height=50)
-button2 = Button(root, text="Enviar Valores")
+button2 = Button(root, text="Reconocer Valores")
 button2.place(x=450,y=50,width=100,height=50)
 button3 = Button(root, text="Exit",command=exitclick)
 button3.place(x=850,y=50,width=100,height=50)
+button2 = Button(root, text="Enviar Valores")
+button2.place(x=450,y=100,width=100,height=50)
+
 # Display Buttons
 #button1_canvas = canvas1.create_window(100, 10,
 #                                       anchor="nw",
