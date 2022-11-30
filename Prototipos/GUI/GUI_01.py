189ce580c70d3b5bda32e61c23be492b994e0364
SSGR18483 <gal18483@uvg.edu.gt>
2022-11-29 18:58:06 -0600

    Update GUI_01.py
    se agregaron funciones y se definió la aplicación lista para utilizarse en conjunto con los demas archivos.

--- Prototipos/GUI/GUI_01.py
+++ Prototipos/GUI/GUI_01.py
@@ -8,21 +8,45 @@
 import numpy
 from PIL import ImageTk
 from PIL import Image
+from PIL import ImageGrab
 import PIL as PIL
+import tkinter as tk
 from tkinter import *
 from tkinter.ttk import *
-import os
+import os,time,threading
 root = Tk()
 root.geometry("1000x750")
 
 img=PIL.Image.open("D:/Documentos/UVG/QUINTO AÑO/Segundo Semestre/Diseño e innovación/GIT/Optimizacion-del-reconocimiento-de-variables-de-Varioguide/Prototipos/Imagenes/Imagenes_gui/brain.jpg")
 # img=PIL.Image.open("C:/Users/galic/Documents/Diseño/GIT/Trabajo-de-Graduaci-n-SG18483/Prototipos/Imagenes/Imagenes_gui/brain.jpg")
 bg = ImageTk.PhotoImage(img)
+cortada = PIL.Image.open("D:/Documentos/UVG/QUINTO AÑO/Segundo Semestre/Diseño e innovación/GIT/Optimizacion-del-reconocimiento-de-variables-de-Varioguide/Prototipos/OCR1/cutted.jpg")
+recortetk = ImageTk.PhotoImage(cortada)
 
 #funciones de los clicks
+#salir
 def exitclick():
     root.destroy()
+#limpiar
+def cleart():
+    mytext1.delete(1.0,END)
 
+def mostrarJ():
+    Joint = 2
+    ADJ1 = -0.2
+    ADJ2 = 0
+    if Joint == 1 or Joint == 3:
+        oracion = f"La junta {Joint} tiene\n-Un ajuste de grado de: {ADJ1}"
+    elif Joint == 2:
+        oracion = f"La junta {Joint} tiene\n-Un ajuste de ángulo de: {ADJ1}º \n-Un ajuste de desplazamiento de: {ADJ2}mm"
+    else:
+        oracion = f"No se ha leido ninguna junta o ajuste."
+    mytext1.insert(0.1,str(oracion))
+
+def showimagencort():
+    imgbox = Text(root, width=25, height=5)
+    imgbox.place(x=0, y=660)
+    imgbox.window_create(tk.END, window=tk.Label(imgbox, image=recortetk))
 # Create Canvas
 canvas1 = Canvas(root, width=1000,height=750)
 canvas1.pack(fill="both", expand=True)
@@ -32,16 +56,26 @@ canvas1.create_image(0, 0, image=bg,
 # Add Text
 canvas1.create_text(500, 25, text="Sistema OCR HUMANA ",font =("Courier", 30,'bold'),fill="black")
 
+#crear widget de texto
+mytext1 = Text(root, width= 40, height=5,font=("Tahoma",12,'bold'))
+mytext1.place(x=300,y=345)
+
+
 # Create Buttons
 
+
 button1 = Button(root, text="Determinar tipo de Junta")
-button1.place(x=50,y=50,width=100,height=50)
-button2 = Button(root, text="Reconocer Valores")
-button2.place(x=450,y=50,width=100,height=50)
-button3 = Button(root, text="Exit",command=exitclick)
-button3.place(x=850,y=50,width=100,height=50)
-button2 = Button(root, text="Enviar Valores")
-button2.place(x=450,y=100,width=100,height=50)
+button1.place(x=25,y=50,width=150,height=50)
+button2 = Button(root, text="Reconocer Valores",command=mostrarJ)
+button2.place(x=440,y=50,width=130,height=50)
+button3 = Button(root, text="Salir",command=exitclick)
+button3.place(x=900,y=700,width=100,height=50)
+button4 = Button(root, text="Enviar Valores")
+button4.place(x=850,y=50,width=100,height=50)
+button5 = Button(root, text="Limpiar",command= cleart)
+button5.place(x=705,y=395,width=50,height=50)
+button5 = Button(root, text="Mostar digito",command=showimagencort )
+button5.place(x=25,y=150,width=50,height=50)
 
 # Display Buttons
 #button1_canvas = canvas1.create_window(100, 10,
